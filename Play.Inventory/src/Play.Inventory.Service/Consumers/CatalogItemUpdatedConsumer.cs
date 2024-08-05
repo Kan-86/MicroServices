f6d61917a682c63b791f54f54d58aa5d3cf4ec2e
Kristófer Andersen <andersen19@gmail.com>
2024-08-05 11:12:49 +0200

    share

---
+++ Play.Inventory/src/Play.Inventory.Service/Consumers/CatalogItemUpdatedConsumer.cs
@@ -0,0 +1,40 @@
+﻿using MassTransit;
+using Play.Common;
+using Play.Inventory.Service.Entities;
+using System.Threading.Tasks;
+using static Play.Catalog.Contracts.Contracts;
+
+namespace Play.Inventory.Service.Consumers;
+public class CatalogItemUpdatedConsumer : IConsumer<CatalogItemUpdated>
+{
+    private readonly IRepository<CatalogItem> repository;
+
+    public CatalogItemUpdatedConsumer(IRepository<CatalogItem> repository)
+    {
+        this.repository = repository;
+    }
+
+    public async Task Consume(ConsumeContext<CatalogItemUpdated> context)
+    {
+        var message = context.Message;
+
+        var item = await repository.GetAsync(message.ItemId);
+
+        if (item is null)
+        {
+            item = new CatalogItem
+            {
+                Id = message.ItemId,
+                Description = message.Description,
+                Name = message.Name
+            };
+
+            await repository.CreateAsync(item);
+            return;
+        }
+        item.Name = message.Name;
+        item.Description = message.Description;
+
+        await repository.CreateAsync(item);
+    }
+}
